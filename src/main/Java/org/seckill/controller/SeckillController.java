13e4ec36f6c58f18675d5d9f5ed0f1818875a88f
Dennis <dennis_zhouheroic@163.com>
2017-07-18 17:59:53 +0800

    Spring mvc配置

---
+++ src/main/Java/org/seckill/controller/SeckillController.java
@@ -0,0 +1,19 @@
+package org.seckill.controller;
+
+import org.seckill.service.SeckillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Dennis_zhou on 2017/7/18.
+ */
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+
+    @Autowired
+    private SeckillService seckillService;
+
+
+}
